35b7417384786d6906a1148ab5f20441f842d85f
Hyperdriveguy <hyperdriveguy@gmail.com>
2020-05-11 13:16:25 -0700

    Separate out command line specific stuff from the called script This makes it so it can be imported into the server module

--- muse2pokecrystal.py
+++ muse2pokecrystal.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-The main function and argument parsing for Muse2pokecrystal.
+The main Muse2pokecrystal program for execution.
 
 This module is a part of Muse2pokecrystal.
 
@@ -21,58 +21,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 Full plain text license https://www.gnu.org/licenses/agpl-3.0.txt
 """
-
-
-import argparse
-from os.path import isfile as filehere
-from src import text, fileio
-import muse2pokecrystal as legacy
-
-TERM_TEXT = text.TerminalText(False)
-
-
-def main():
-    parser = argparse.ArgumentParser(description=TERM_TEXT.prog_description)
-    parser.add_argument('musicxml',
-                        help=TERM_TEXT.arg_musicxml_desc)
-    parser.add_argument('asm',
-                        help=TERM_TEXT.arg_asm_desc)
-    parser.add_argument('-c', '--config',
-                        help=TERM_TEXT.arg_config_desc)
-    parser.add_argument('-t', '--tempo',
-                        type=int,
-                        help=TERM_TEXT.arg_tempo_desc)
-    parser.add_argument('-n', '--name',
-                        metavar='"SONG NAME"',
-                        help=TERM_TEXT.arg_name_desc)
-    parser.add_argument('-N', '--noiseless',
-                        action='store_true',
-                        help=TERM_TEXT.arg_noiseless_desc)
-    parser.add_argument('-f', '--overwrite',
-                        action='store_true',
-                        help=TERM_TEXT.arg_overwrite_desc)
-    parser.add_argument('-l', '--custom-loop',
-                        action='store_true',
-                        help=TERM_TEXT.arg_custom_loop_desc)
-    parser.add_argument('-C', '--colored-output',
-                        action='store_true',
-                        help=TERM_TEXT.arg_colored_output_desc)
-    parser.add_argument('-x', '--no-optimizations',
-                        action='store_true',
-                        help=TERM_TEXT.arg_no_optimizations_desc)
-    parser.add_argument('-v', '--version', action='version',
-                        version=TERM_TEXT.version)
-    args = parser.parse_args()
-    if filehere(args.asm) and args.overwrite is False:
-        confirm = input(args.asm +
-                        text.TerminalText
-                        (args.colored_output)
-                        .overwrite_prompt)
-        if confirm in ['y', 'Y']:
-            fileio.write_music_file(args)
-    else:
-        fileio.write_music_file(args)
-
+from src import commandline
 
 if __name__ == "__main__":
-    main()
+    commandline.parse_arguments()
